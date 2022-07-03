2aaf89da1b3bb055d0cd5d7070e6dd71197d0fc6
Анатолий Кузин <kuzin.95@list.ru>
2022-07-03 14:25:46 +0300

    mk12

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -103,6 +103,8 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = [BASE_DIR + '/api_yamdb/static/']
+
 AUTH_USER_MODEL = 'users.CustomUser'
 
 REST_FRAMEWORK = {
